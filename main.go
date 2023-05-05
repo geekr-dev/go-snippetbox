5d258d92e1b9c4b019dbb748e4f995de4233fd63
geekr <geekr@hey.com>
2023-05-05 13:06:13 +0800

    custom http headers

--- main.go
+++ main.go
@@ -18,6 +18,13 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		//w.WriteHeader(405)
+		//w.Write([]byte("Method Not Allowed"))
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
