0e7d96aac1af06926bcef04eae192424eeed7a40
geekr <geekr@hey.com>
2023-05-05 20:03:22 +0800

    session & flash message

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -39,7 +39,6 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
 	app.render(w, http.StatusOK, "view.tmpl", data)
@@ -86,6 +85,8 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
